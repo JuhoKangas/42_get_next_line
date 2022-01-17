eb9b3039eb6474c49dcf97267d50273b97d5939d
JuhoKangas <jkangas@student.hive.fi>
2022-01-17 12:34:53 +0200

    add buff size

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,15 @@
 /*   By: jkangas <jkangas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 17:02:30 by jkangas           #+#    #+#             */
-/*   Updated: 2022/01/13 18:24:11 by jkangas          ###   ########.fr       */
+/*   Updated: 2022/01/14 12:10:31 by jkangas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
+#include "get_next_line.h"
 
 int	get_next_line(const int fd, char **line)
 {
-
+	char	*buffer[BUFF_SIZE + 1];
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,15 @@
 /*   By: jkangas <jkangas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 17:04:55 by jkangas           #+#    #+#             */
-/*   Updated: 2022/01/13 18:24:08 by jkangas          ###   ########.fr       */
+/*   Updated: 2022/01/14 12:10:28 by jkangas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 4096
+# define FD_SIZE
 
 int	get_next_line(const int fd, char **line);
 
